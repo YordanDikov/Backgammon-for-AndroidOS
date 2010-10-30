933241d9e8849d97939a8c579dc39496bfb0d42e
YordanDikov <jordanidikov@gmail.com>
2010-10-30 20:56:01 +0300

    Project template.
    Signed-off-by: YordanDikov <jordanidikov@gmail.com>

---
+++ src/fmi/android/game/backgammon_for_androidos/Backgammon.java
@@ -0,0 +1,12 @@
+package fmi.android.game.backgammon_for_androidos;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Backgammon extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
